946891caba621145536eb788e0fb5d12560b83ac
liugongjian <liugongjianxin@163.com>
2021-02-18 16:45:50 +0800

    vcp-37

--- src/views/role/roleUI/index.js
+++ src/views/role/roleUI/index.js
@@ -84,7 +84,7 @@ class RoleUI extends Component {
                     <div style={{padding:'0 20px 0 0'}}>
                       <div style={{display:'inline-block',margin:'5px 0 0 5px'}}>
                         <img style={{width:"20px",height:"20px",display:"inline-block"}} src={infoPic}/>
-                        <span>请配置当前角色能够访问的系统管理菜单</span>
+                        <span>请配置当前角色能够访问的{this.props.activeMenuKey.key==='1' ? "系统管理菜单" : "摄像头点位权限"}</span>
                       </div>
                       <Search placeholder={this.props.activeMenuKey.key==='1' ? "请输入菜单名称" : "请输入区域名称"} style={{ display:'inline-block',float:'right',width:'25%',margin:'10px 20px 0 0'}} 
                       onChange={(e)=>this.setState({searchValue:e.target.value})}
