6d6795cb356a22f099b8a78c138ffc95581c6c7b
Miro <ak_._47@qq.com>
2021-03-01 19:02:15 +0800

    样式修改

--- src/views/alarms/Modals.js
+++ src/views/alarms/Modals.js
@@ -239,7 +239,10 @@ const ImageModal = ({
     visible={visible}
     footer={null}
     onCancel={closeModal}
-    width="40vw"
+    width="50vw"
+    style={{
+      maxWidth: '800px'
+    }}
   >
     <img
       src={src}
