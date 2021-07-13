957757269fc5ac7bc2f64aa24e5a896c03c85da7
Miro <ak_._47@qq.com>
2021-07-13 14:06:00 +0800

    筛选条件修改

--- src/views/intelligentSearch/face.js
+++ src/views/intelligentSearch/face.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {
   Input, Select, Upload, Icon, message, Button, Radio, Dropdown, Menu,
-  Spin, Form, Slider, DatePicker
+  Spin, Form, Slider, DatePicker, Cascader
 } from 'antd';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -11,6 +11,9 @@ import {
 } from 'Redux/reducer/intelligentSearch';
 import NODATA_IMG from 'Assets/nodata.png';
 import { resizeDependencies, trueDependencies } from 'mathjs';
+import {
+  getDeviceTree
+} from 'Redux/reducer/alarms';
 import PeopleRes from './results/peopleRes';
 import CarRes from './results/carRes';
 import ImagePicker from './imagePicker';
@@ -37,7 +40,7 @@ const mapStateToProps = state => ({
   nextPlateImageId: state.intelligentSearch.nextPlateImageId,
 });
 const mapDispatchToProps = dispatch => bindActionCreators(
-  { searchPlate, searchFace },
+  { searchPlate, searchFace, getDeviceTree },
   dispatch
 );
 
@@ -52,12 +55,75 @@ class IntelligentSearch extends Component {
       pageSize: 10,
       total: 0,
       current: 1,
+      deviceTree: [],
+      devicesLoading: false,
       // searchType: SEARCH_TYPES_PLATE,
     };
   }
 
   componentDidMount() {
+    this.initData();
   }
+
+  initData = () => {
+    this.setState({ devicesLoading: true });
+    this.props.getDeviceTree().then((res) => {
+      console.log('getDeviceTree', res);
+      const tree = this.dataToTree(res);
+      console.log('DeviceTree', tree);
+      this.setState({
+        devicesLoading: false,
+        deviceList: res,
+        deviceTree: tree
+      });
+    }).catch((err) => {
+      this.setState({
+        devicesLoading: false,
+      });
+    });
+  }
+
+  dataToTree = (data) => {
+    // 下面的forEach写法会改变原数组，所以深度拷贝一次
+    const copy = JSON.parse(JSON.stringify(data));
+    const map = {};
+    copy.forEach((item) => {
+      item.label = item.name;
+      // if (item.type == 0) {
+      //   item.label = item.name;
+      // } else {
+      //   item.label = (
+      //     <span>
+      //       <EIcon type="myicon-monitorIcon" />
+      //       {item.name}
+      //     </span>
+      //   );
+      // }
+      item.value = item.id;
+      map[item.id] = item;
+    });
+    const val = [];
+    copy.forEach((item) => {
+      const parent = map[item.pid];
+      if (parent) {
+        (parent.children || (parent.children = [])).push(item);
+      } else {
+        val.push(item);
+      }
+    });
+    // 子节点为区域，不是设备，不可选
+    // const setAreaNodeDisabled = (tree) => {
+    //   tree.forEach((item) => {
+    //     if (item.children) {
+    //       setAreaNodeDisabled(item.children);
+    //     } else {
+    //       item.disabled = item.type == 0;
+    //     }
+    //   });
+    // };
+    // setAreaNodeDisabled(val);
+    return val;
+  };
 
  handleSearch = () => {
    const {
@@ -156,7 +222,8 @@ class IntelligentSearch extends Component {
 
  render() {
    const {
-     resData, resLoading, curImage, filterType, total, current, pageSize
+     resData, resLoading, curImage, filterType, total, current, pageSize,
+     deviceTree, devicesLoading,
    } = this.state;
    const { getFieldDecorator } = this.props.form;
    const formItemLayout = {
@@ -247,11 +314,13 @@ class IntelligentSearch extends Component {
                  rules: [
                  ],
                })(
-                 <Select>
-                   <Option value="1">设备1</Option>
-                   <Option value="2">设备2</Option>
-                   <Option value="3">设备3</Option>
-                 </Select>
+                 <Cascader
+                   changeOnSelect
+                   placeholder="请选择设备"
+                   popupClassName={styles.cameraCascader}
+                   options={deviceTree}
+                   allowClear={false}
+                 />
                )}
              </Form.Item>
              <Form.Item label="时间范围">
